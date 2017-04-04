18a950687501fbd6b68e78555e90dbba1aab6d65
Fagner Lima <contato@fagnerlima.pro.br>
2017-04-04 10:12:11 -0300

    [UPDATE] Refactor export files in index

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,6 @@
  * @author Fagner Lima
  * @since 1.0.2
  */
-export * from './src';
+export { NgMaskModule }  from './src/ng-mask.module';
+export { MaskDirective } from './src/mask.directive';
+export { MaskOptions }   from './src/mask-options';
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 "use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
 /**
  * @author Fagner Lima
  * @since 1.0.2
  */
-__export(require('./src'));
+var ng_mask_module_1 = require('./src/ng-mask.module');
+exports.NgMaskModule = ng_mask_module_1.NgMaskModule;
+var mask_directive_1 = require('./src/mask.directive');
+exports.MaskDirective = mask_directive_1.MaskDirective;
 //# sourceMappingURL=index.js.map
