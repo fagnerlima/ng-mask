f1359a2ed243ed932569a422e52b77f1871d6754
Fagner Lima <contato@fagnerlima.pro.br>
2017-06-08 20:13:54 -0300

    [UPDATE] Updates Predefined Type Masks

--- src/strategy/mask-factory/predefined-type-mask-factory.ts
+++ src/strategy/mask-factory/predefined-type-mask-factory.ts
@@ -2,18 +2,18 @@ import { MaskFactory } from './mask-factory';
 import { Mask } from '../../mask';
 
 const predefinedMasks: { [type: string]: Mask } = {
-  'br-celular': new Mask('(00) 90000-0000'),
-  'br-cep': new Mask('00000-000'),
-  'br-data': new Mask('00/00/0000'),
-  'br-cnpj': new Mask('00.000.000/0000-00'),
-  'br-cpf': new Mask('000.000.000-00'),
-  'br-moeda': new Mask('#.##0,00', { reverse: true }),
-  'br-telefone': new Mask('(00) 90000-0000'),
-  'us-phone': new Mask('(000) 000-0000'),
-  'coordinate-lat': new Mask('000º00.0000\'~', { translation: { '~': { pattern: /[N|S]/ } } }),
-  'coordinate-long': new Mask('000º00.0000\'~', { translation: { '~': { pattern: /[N|S]/ } } }),
+  'brCelular': new Mask('(00) 90000-0000'),
+  'brCep': new Mask('00000-000'),
+  'brCnpj': new Mask('00.000.000/0000-00'),
+  'brCpf': new Mask('000.000.000-00'),
+  'brData': new Mask('00/00/0000'),
+  'brMoeda': new Mask('#.##0,00', { reverse: true }),
+  'brTelefone': new Mask('(00) 90000-0000'),
+  'usPhone': new Mask('(000) 000-0000'),
+  'coordinateLat': new Mask('000º00.0000\'~', { translation: { '~': { pattern: /[N|S]/ } } }),
+  'coordinateLong': new Mask('000º00.0000\'~', { translation: { '~': { pattern: /[N|S]/ } } }),
   'date': new Mask('00/00/0000'),
-  'date-time': new Mask('00/00/0000 00:00:00'),
+  'dateTime': new Mask('00/00/0000 00:00:00'),
   'time': new Mask('00:00:00')
 };
 
